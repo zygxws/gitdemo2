dcf4ca0476a33317d62faa82edb399011684619c
zrs <401919274@qq.com>
2021-07-27 20:54:51 +0800

    master 1

--- src/main/java/gitd/gittext.java
+++ src/main/java/gitd/gittext.java
@@ -5,5 +5,6 @@ public class gittext {
         System.out.println("hello git!");
         System.out.println("hello git2!");
         System.out.println("hello git4!");
+        System.out.println("hello git5!");
     }
 }
