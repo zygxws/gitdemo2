4612524e65a0a06e1c54621e6d23380657c7bf9e
zygxws <87938746+zygxws@users.noreply.github.com>
2021-07-27 22:08:38 +0800

    pull commit

--- src/main/java/gitd/gittext.java
+++ src/main/java/gitd/gittext.java
@@ -8,5 +8,6 @@ public class gittext {
         System.out.println("hello git5!");
         System.out.println("hello git6!");
         System.out.println("push");
+        System.out.println("pull");
     }
 }
